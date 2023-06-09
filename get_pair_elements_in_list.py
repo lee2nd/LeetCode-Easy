be81e27600456c932019db080c30af8066d9d315
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-09 15:52:49 +0800

    Update get_pair_elements_in_list.py

--- get_pair_elements_in_list.py
+++ get_pair_elements_in_list.py
@@ -1,7 +1,12 @@
-#  get each pair of adjacent elements in a list
-
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+#  get each pair of adjacent elements in a list
 for i in range(len(my_list) - 1):
     pair = (my_list[i], my_list[i+1])
     print(pair)
+
+#  get every combination of 2 elements in a list    
+for i in range(len(my_list)):
+    for j in range(i+1, len(my_list)):
+        pair = (my_list[i], my_list[j])
+        print(pair)    
