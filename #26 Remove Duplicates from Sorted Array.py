1fd1cbd76be7222ed91fa7a2a240a3082721f1a8
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-13 13:49:47 +0800

    Update #26 Remove Duplicates from Sorted Array.py

--- #26 Remove Duplicates from Sorted Array.py
+++ #26 Remove Duplicates from Sorted Array.py
@@ -1,5 +1,6 @@
 # In short, without [:], we're creating a new list object, which is against what this problem is asking for:
 # "Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory."
+# 從頭到尾只會有一個 list 變數
 
 # modifying the input array in-place
 nums[:] = sorted(set(nums))
