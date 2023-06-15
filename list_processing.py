b6245512d7b2bf535e2d5749689e5c4e43c0ce3f
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-15 11:01:34 +0800

    Update list_processing.py

--- list_processing.py
+++ list_processing.py
@@ -1,9 +1,11 @@
 # remove all occurrences of a value from a list
 # https://stackoverflow.com/questions/1157106/remove-all-occurrences-of-a-value-from-a-list
 # list.remove(2) 只能一次移掉一個(從最前面開始移)
-
 x = [1,2,3,2,2,2,3,4]
 x = [i for i in x if i != 2]
 print(x)
-
 # [1, 3, 3, 4]
+
+# shortest string in list
+min(['Alice', 'Bob', 'Pete',], key=len)
+# 'Bob'
