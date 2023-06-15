5b58ee44b885867c3b9f36cc65f0490ae30a01cf
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-15 15:39:51 +0800

    Create dict_processing.py

---
+++ dict_processing.py
@@ -0,0 +1,18 @@
+# 把一個 list 當成 dict 的 keys
+d = {}
+for i in keyList:
+    d[i] = None
+
+# 取得擁有最大 value 的那個 key
+ages = {
+    'Matt': 30,
+    'Katie': 29,
+    'Nik': 31,
+    'Jack': 43,
+    'Alison': 32,
+    'Kevin': 38
+}
+
+max_value = max(ages, key=ages.get)
+print(max_value)
+# Jack
