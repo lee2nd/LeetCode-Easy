2fbbc38aac45390a6c940738c88d234b3fde913d
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-23 19:32:15 +0800

    Create two_pointer.py

---
+++ two_pointer.py
@@ -0,0 +1,14 @@
+# https://towardsdatascience.com/two-pointer-approach-python-code-f3986b602640
+# Two are better than one if they act as one.
+# data not necessarily sorting
+# This approach is generally used to search pairs in a sorted array
+# Space complexity : O(1)
+
+# key points
+## Pointer Initialization
+## Pointer Movement
+## Stop Condition
+
+# Direction 
+## Same
+## Opposite 
