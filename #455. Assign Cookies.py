c4e96fcff689e05b0457ffdcf287414477546d8b
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-26 16:58:56 +0800

    Update #455. Assign Cookies.py

--- #455. Assign Cookies.py
+++ #455. Assign Cookies.py
@@ -2,7 +2,7 @@ class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g = sorted(g)
         s = sorted(s)
-        cookie, kid, count = 0, 0, 0
+        cookie, kid = 0, 0
         while cookie < len(s) and kid < len(g):
             if s[cookie] >= g[kid]:
                 cookie += 1
