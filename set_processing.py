82d333372ddb2e441d6db1c695573f248eb6e441
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-27 11:02:30 +0800

    Create set_processing.py

---
+++ set_processing.py
@@ -0,0 +1,9 @@
+# Initialize a Python set
+set()
+
+# Quick Examples of Joining Two Sets
+# Join using |
+myset = myset1 | myset2 | myset3
+
+# Join using union()
+myset = myset1.union(myset2,myset3)
