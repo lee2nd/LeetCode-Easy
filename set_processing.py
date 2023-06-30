542a655a77a4360774b85502a2ef4f17a4c2b0f1
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-07-01 00:54:03 +0800

    Update set_processing.py

--- set_processing.py
+++ set_processing.py
@@ -13,3 +13,12 @@ myset = myset1.union(myset2,myset3)
 # True
 {1,5} <= {4,1,2}
 # False
+
+# append values to a set
+a.add(1)
+a.add(2)
+a.update([3, 4])
+# {1, 2, 3, 4}
+
+# remove values to a set
+myset.remove("1")
