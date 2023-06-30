52dec11b350cd0b046fc4434f5c980457e1453b4
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-06-30 15:39:53 +0800

    Update fundamental.py

--- fundamental.py
+++ fundamental.py
@@ -3,3 +3,14 @@
 float("inf")
 # -∞
 float(-inf)
+
+# assigning multiple values in an inline if/else statement
+# https://stackoverflow.com/questions/67277134/assigning-multiple-values-in-an-inline-if-else-statement
+x = y = 0
+value = 11
+threshold = 5
+x, y = (x+1, 0) if value <= threshold else (x-1, value)
+x
+# -1
+y
+# 11
