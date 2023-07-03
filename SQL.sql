5a8eeec6e02ad676b7b97581810eee171081782c
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-07-04 02:25:28 +0800

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -3,7 +3,7 @@
 select
   (Your entire current Select statement goes here) as Alias
 
-# sex column 的 m,f 交換
+-- sex column 的 m,f 交換
 Salary table:
 +----+------+-----+--------+
 | id | name | sex | salary |
@@ -19,3 +19,9 @@ CASE sex
     WHEN 'm' THEN 'f'
     ELSE 'm'
 END;
+
+-- 在 SQL 的 GROUP BY 子句中，可以使用聚合函數（Aggregate Function）來對分組的數據進行計算，包括 SUM、AVG、COUNT、MIN、MAX 等。如果需要在 GROUP BY 子句中進行相減的操作
+
+SELECT user_id, MAX(price) - MIN(price) as diff
+FROM orders
+GROUP BY user_id;
