f81673656b4f64c2fd4afca7148cd037caff6e4a
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-07-03 11:51:01 +0800

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -2,3 +2,20 @@
 -- How to return NULL when result is empty
 select
   (Your entire current Select statement goes here) as Alias
+
+# sex column 的 m,f 交換
+Salary table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
++----+------+-----+--------+
+
+UPDATE salary SET sex =
+CASE sex
+    WHEN 'm' THEN 'f'
+    ELSE 'm'
+END;
