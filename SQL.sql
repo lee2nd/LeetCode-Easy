718ce40f07e0844a0101df4c7d4c29aefdabeaaa
Yu-Cheng(Frank) Lee <48121658+lee2nd@users.noreply.github.com>
2023-07-03 11:36:54 +0800

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,4 @@
+-- https://stackoverflow.com/questions/17250243/how-to-return-null-when-result-is-empty
+-- How to return NULL when result is empty
+select
+  (Your entire current Select statement goes here) as Alias
