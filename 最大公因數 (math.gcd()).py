27eb2ecbdb4fe5f2f8cdc327514595fb1e54cb2c
Lee <48121658+lee2nd@users.noreply.github.com>
2023-07-24 15:54:35 +0800

    Create 最大公因數 (math.gcd()).py

---
+++ 最大公因數 (math.gcd()).py
@@ -0,0 +1,23 @@
+# https://www.w3schools.com/python/ref_math_gcd.asp
+
+#Import math Library
+import math
+
+#find the  the greatest common divisor of the two integers
+print (math.gcd(3, 6))
+print (math.gcd(6, 12))
+print (math.gcd(12, 36))
+print (math.gcd(-12, -36))
+print (math.gcd(5, 12))
+print (math.gcd(10, 0))
+print (math.gcd(0, 34))
+print (math.gcd(0, 0))
+
+# 3
+# 6
+# 12
+# 12
+# 1
+# 10
+# 34
+# 0
