6e5b093cafd9fcd180652183614580cb7f459949
Lee <48121658+lee2nd@users.noreply.github.com>
2023-07-27 10:35:43 +0800

    Create # 1037. Valid Boomerang.py

---
+++ # 1037. Valid Boomerang.py
@@ -0,0 +1,11 @@
+# https://stackoverflow.com/questions/3813681/checking-to-see-if-3-points-are-on-the-same-line
+
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        x1, y1 = points[0]
+        x2, y2 = points[1]
+        x3, y3 = points[2]
+        
+        area = abs(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))/2
+      
+        return area != 0
