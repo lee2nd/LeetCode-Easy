4e9c1c79dffffcaafcfcf087243222785d5659b5
Lee <48121658+lee2nd@users.noreply.github.com>
2023-08-17 10:51:54 +0800

    Update # 1854. Maximum Population Year.py

--- # 1854. Maximum Population Year.py
+++ # 1854. Maximum Population Year.py
@@ -3,29 +3,30 @@ class Solution:
         # the timespan 1950-2050 covers 101 years
         delta = [0] * 101
 
-		    # to make explicit the conversion from the year (1950 + i) to the ith index
+		# to make explicit the conversion from the year (1950 + i) to the ith index
         conversionDiff = 1950 
 		
         for l in logs:
-			  # the log's first entry, birth, increases the population by 1
+			# the log's first entry, birth, increases the population by 1
             delta[l[0] - conversionDiff] += 1 
 			
-			  # the log's second entry, death, decreases the population by 1
+			# the log's second entry, death, decreases the population by 1
             delta[l[1] - conversionDiff] -= 1
 
         runningSum = 0
         maxPop = 0
         year = 1950
 		
-		    # find the year with the greatest population
+		# find the year with the greatest population
         for i, d in enumerate(delta):
             runningSum += d
 			
-			  # since we want the first year this population was reached, only update if strictly greater than the previous maximum population
+			# since we want the first year this population was reached, only update if strictly greater than the previous maximum population
             if runningSum > maxPop:
                 maxPop = runningSum
                 year = conversionDiff + i
 				
         return year
+# 觀念 : 一出生那年"之後"就多一個人，一死亡那年"之後"就少一個人
 # test case : logs = [[1950,1961],[1960,1971],[1970,1981]]
 # [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  
