84b6e5864c2e5b65a2674baa555c26195d63f161
Lee <48121658+lee2nd@users.noreply.github.com>
2023-10-24 15:07:52 +0800

    Update and rename itertools.product.py to itertools.py

---
+++ itertools.py
@@ -0,0 +1,28 @@
+# https://docs.python.org/3/library/itertools.html
+
+import itertools
+
+for i,j in itertools.product(range(3), range(3)):
+    print(i,j)
+
+# 0 0
+# 0 1
+# 0 2
+# 1 0
+# 1 1
+# 1 2
+# 2 0
+# 2 1
+# 2 2
+
+product('ABCD', repeat=2)
+# AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD
+
+permutations('ABCD', 2)
+# AB AC AD BA BC BD CA CB CD DA DB DC
+
+combinations('ABCD', 2)
+# AB AC AD BC BD CD
+
+combinations_with_replacement('ABCD', 2)
+# AA AB AC AD BB BC BD CC CD DD
